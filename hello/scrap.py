996611b78ef759a9dcc8fce697b6119ff4222a1a
Ai7mn <50971079+Ai7mn@users.noreply.github.com>
2023-02-07 01:49:28 +0300

    updateww

---
+++ hello/scrap.py
@@ -0,0 +1,127 @@
+import mysql.connector
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import sys
+import os
+
+mydb = mysql.connector.connect(host="188.166.2.179",
+                               user="aiman",
+                               password="Ayman1@1",
+                               port="3306",
+                               database="phone_link")
+
+my_cursor = mydb.cursor(buffered=True)
+sql = "SELECT * FROM phone_link where phone_id is NULL ORDER BY id DESC LIMIT 60"
+
+my_cursor.execute(sql)
+
+my_results = my_cursor.fetchall()
+
+
+# os.environ['HTTP_PROXY'] = "http://p.webshare.io:80:theaiman-1:theahmed"
+
+def insert_phone_name(title, phone_id):
+    insert_query = "INSERT INTO phones (the_name)  VALUES (%s)"
+    insert_val = (str(title),)
+    my_cursor.execute(insert_query, insert_val)
+    print("done1")
+    mydb.commit()
+    sql = "UPDATE phone_link SET phone_id = %s WHERE id = %s"
+    vals = (int(my_cursor.lastrowid), int(phone_id))
+    my_cursor.execute(sql, vals)
+    mydb.commit()
+    print("done2")
+    return str(my_cursor.lastrowid)
+
+
+def insert_phone_specs(phone, spec, title, the_val):
+    insert_query = "INSERT INTO phone_spec (phone, spec, title, the_val)  VALUES (%s,%s,%s,%s)"
+    insert_val = (int(phone), spec, title, the_val)
+    my_cursor.execute(insert_query, insert_val)
+    mydb.commit()
+    print("done3")
+
+
+# def take(phone_id, url):
+#     try:
+#         page = urllib.request.urlopen(url)
+#     except Exception as x:
+#         print(f"the problem is : {x}")
+#     soup = BeautifulSoup(page.text, 'html.parser')
+#     try:
+#         phone_name = soup.find('h1').text
+#     except:
+#         phone_name = "Could not find"
+#     print(phone_name)
+#
+#     tables = soup.find_all("table")
+#
+#     for table in tables:
+#         header = [th.text for th in table.find_all('th')]
+#         try:
+#             the_header = str(header[0])
+#         except:
+#             the_header = "None"
+#         for tr in table.find_all('tr'):
+#             data = []
+#             for td in tr.find_all('td'):
+#                 data.append(td.text)
+#             try:
+#                 data1 = str(data[0])
+#             except:
+#                 data1 = "None"
+#             try:
+#                 data2 = str(data[1])
+#             except:
+#                 data2 = "None"
+#         insert_phone_specs(int(new_phone_id), the_header, data1, data2)
+#     print(f"done : {url}")
+#
+
+print("start")
+for x in my_results:
+    print(x[2])
+    print(f" id is : {x[0]} and link is : {x[2]}")
+    the_url = str(x[2])
+    phone_id = int(x[0])
+    try:
+        page = urllib.request.urlopen(the_url)
+    except Exception as x:
+        print(f"the problem is : {x}")
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
+    try:
+        phone_name = soup.find('h1').text
+    except:
+        phone_name = "Could not find"
+    print(phone_name)
+    insert_phone_name(phone_name, phone_id)
+    sql = "SELECT * FROM phones ORDER BY id DESC LIMIT 1"
+
+    my_cursor.execute(sql)
+
+    new_phone_id = my_cursor.fetchone()
+    print(str(new_phone_id[0]))
+    tables = soup.find_all("table")
+
+    for table in tables:
+        header = [th.text for th in table.find_all('th')]
+        try:
+            the_header = str(header[0])
+        except:
+            the_header = "None"
+        for tr in table.find_all('tr'):
+            data = []
+            for td in tr.find_all('td'):
+                data.append(td.text)
+            try:
+                data1 = str(data[0])
+            except:
+                data1 = "None"
+            try:
+                data2 = str(data[1])
+            except:
+                data2 = "None"
+        insert_phone_specs(int(new_phone_id[0]), the_header, data1, data2)
+    print(f"done : {the_url}")
--- hello/views.py
+++ hello/views.py
@@ -16,7 +16,6 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic.edit import FormView
 # Create your views here.
-from rembg import remove
 
 from gettingstarted.custom_storage import MediaStorage
 from .forms import AddForm
@@ -28,6 +27,7 @@ media_storage = MediaStorage()
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    os.system('python3 scrap.py')
     return render(request, "home.html")
 
 
@@ -47,222 +47,222 @@ def get_random_string(length):
     print("Random string of length", length, "is:", result_str)
     return result_str
 
-
-class FileFieldFormView(FormView):
-    form_class = AddForm
-    template_name = 'home.html'  # Replace with your template.
-    success_url = '/'  # Replace with your URL or reverse().
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('images')
-        if form.is_valid():
-            for f in files:
-                print(f)
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-
-def getfiles(request, filenames):
-    filename = 'zip11.zip'
-    # filedir = 'results/zip11.zip'
-    folder = 'results/'
-    baisc_dir = folder + filename
-    filepath = os.path.join(settings.MEDIA_ROOT, folder)
-    with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
-        for image_name in filenames:
-            image_url = filenames[image_name]
-            url = urllib.request.urlopen(image_url)
-            # image = Image.open(requests.get(image_url, stream=True).raw)
-            # img_name = os.path.basename(image_path)
-            print("url is : " + url)
-            img_zip.write(url.read(), image_name)
-
-    img_zip.close()
-
-    file = open(baisc_dir, 'rb')
-    fs = FileSystemStorage(location=filepath)  # defaults to   MEDIA_ROOT
-    file_name = "myimages" + str(get_random_string(8)) + ".zip"
-    file_path_within_bucket = os.path.join("output", file_name)
-    # final_file = fs.save(filename, file)
-    # file_url = fs.url(final_file)
-    media_storage.save(file_path_within_bucket, img_zip)
-    file_url = media_storage.url(file_path_within_bucket)
-    print("zip file url is : " + str(file_url))
-    file.close()
-    # final_path = "/results" + str(file_url)
-    request.session['final_file'] = file_url
-    # return file_url
-
-
-def append_output(image):
-    fname = os.path.splitext(image.name)[0]
-    image_name = str(fname) + ".PNG"
-    output_path = os.path.join(settings.BASE_DIR, 'images', image_name)
-    return output_path
-
-
-def delete_old_files(folder):
-    # folder = loc
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
-def process_image(image):
-    fname = os.path.splitext(image.name)[0]
-    image_name = str(fname) + ".PNG"
-    output_path = os.path.join(settings.BASE_DIR, 'images', image_name)
-    # print("output_path is : "+ str(output_path))
-    input = Image.open(image)
-    output = remove(input)
-    # format = 'png'
-    image_path_within_bucket = os.path.join("images", image_name)
-    print("image_path_within_bucket is : " + image_path_within_bucket)
-    output.save(output_path)
-    image = open(output_path, 'rb')
-    # media_storage.save(image_path_within_bucket, image)
-    # image_url = media_storage.url(image_path_within_bucket)
-    # print("image url is : " + str(image_url))
-    # # bg_removed_images.append(str(output_path))
-    # print(image_url)
-
-
-def form_handle(request):
-    form = AddForm()
-    if request.method == 'POST':
-        form = AddForm(request.POST, request.FILES or None)
-        if form.is_valid():
-            cd = form.cleaned_data
-            # now in the object cd, you have the form as a dictionary.
-            files = request.FILES.getlist('images')
-            # x = 1
-            # y = 1
-            # images_list = {}
-            bg_removed_images = []
-            for image in files:
-                print()
-                bg_removed_images.append(str(append_output(image)))
-            executor = concurrent.futures.ProcessPoolExecutor(2)
-            futures = [executor.submit(process_image, item) for item in files]
-            concurrent.futures.wait(futures)
-            print('converting was done')
-            baisc_dir = 'images5.zip'
-            with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
-                for image_path in bg_removed_images:
-                    print(image_path)
-                    img_zip.write(image_path, basename(image_path))
-                img_zip.close()
-            print('zipping was done')
-            file = open(baisc_dir, 'rb')
-            file_name = "myimages" + get_random_string(8) + ".zip"
-            file_path_within_bucket = os.path.join("output", file_name)
-            media_storage.save(file_path_within_bucket, file)
-            file_url = media_storage.url(file_path_within_bucket)
-            print('uploading was done')
-            print("zip file url is : " + str(file_url))
-            request.session['final_file'] = str(file_url)
-            output_path = os.path.join(settings.BASE_DIR, 'images')
-            delete_old_files(output_path)
-            form = AddForm()
-            # return HttpResponseRedirect(str(file_url))
-            return HttpResponseRedirect(reverse('home'))
-    return render(request, "index.html", {"form": form})
-
-
-def form_handle2(request):
-    form = AddForm()
-    if request.method == 'POST':
-        form = AddForm(request.POST, request.FILES or None)
-        if form.is_valid():
-            cd = form.cleaned_data
-            # now in the object cd, you have the form as a dictionary.
-            files = request.FILES.getlist('images')
-            x = 1
-            y = 1
-            images_list = {}
-            bg_removed_images = []
-            print("first images are: ")
-            x = 1
-            for image in files:
-                images_list[x] = image
-                x = x + 1
-            print(images_list)
-            print(x)
-            for y in range(y, x, 3):
-                y2 = int(y) + 1
-                y3 = int(y) + 2
-                if y3 < x:
-                    t1 = threading.Thread(target=process_image, args=(images_list[y],))
-                    t2 = threading.Thread(target=process_image, args=(images_list[y2],))
-                    t3 = threading.Thread(target=process_image, args=(images_list[y3],))
-                    t1.start(), t2.start(), t3.start()
-                    t1.join(), t2.join(), t3.join()
-                    bg_removed_images.append(str(append_output(images_list[y])))
-                    bg_removed_images.append(str(append_output(images_list[y2])))
-                    bg_removed_images.append(str(append_output(images_list[y3])))
-                elif y2 < x:
-                    t1 = threading.Thread(target=process_image, args=(images_list[y],))
-                    t2 = threading.Thread(target=process_image, args=(images_list[y2],))
-                    t1.start(), t2.start()
-                    t1.join(), t2.join()
-                    bg_removed_images.append(str(append_output(images_list[y])))
-                    bg_removed_images.append(str(append_output(images_list[y2])))
-                else:
-                    t1 = threading.Thread(target=process_image, args=(images_list[y],))
-                    t1.start()
-                    t1.join()
-                    bg_removed_images.append(str(append_output(images_list[y])))
-
-            print('bg_removed_images is ::')
-            print(bg_removed_images)
-            baisc_dir = 'images4.zip'
-            with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
-                for image_path in bg_removed_images:
-                    img_zip.write(image_path)
-                img_zip.close()
-            file = open(baisc_dir, 'rb')
-            file_name = "myimages" + get_random_string(8) + ".zip"
-            file_path_within_bucket = os.path.join("output", file_name)
-            media_storage.save(file_path_within_bucket, file)
-            file_url = media_storage.url(file_path_within_bucket)
-            print("zip file url is : " + str(file_url))
-            request.session['final_file'] = str(file_url)
-            form = AddForm()
-            # return HttpResponseRedirect(str(file_url))
-            return HttpResponseRedirect(reverse('home'))
-    return render(request, "index.html", {"form": form})
-
-
-def downlaod_images(request, file):
-    file_link = file
-    try:
-        file_name = request.session['final_file']
-    except:
-        file_name = None
-    if file_name is None:
-        print("file_name is None")
-        return HttpResponseRedirect(reverse('home'))
-    else:
-        if file_link != file_name:
-            print("file_name is not equal with session name")
-            return HttpResponseRedirect(reverse('home'))
-        else:
-            filepath = os.path.join(settings.MEDIA_ROOT, "results")
-            fs = FileSystemStorage(location=filepath)
-            out_file = fs.path(file_link)
-            the_file3 = open(out_file, 'rb')
-            the_file = "/media/results" + str(out_file)
-            print(" the file is : " + str(the_file))
-            response = FileResponse(the_file3)
-
-            # serv = serve(request, str(the_file))
-            return response
+#
+# class FileFieldFormView(FormView):
+#     form_class = AddForm
+#     template_name = 'home.html'  # Replace with your template.
+#     success_url = '/'  # Replace with your URL or reverse().
+#
+#     def post(self, request, *args, **kwargs):
+#         form_class = self.get_form_class()
+#         form = self.get_form(form_class)
+#         files = request.FILES.getlist('images')
+#         if form.is_valid():
+#             for f in files:
+#                 print(f)
+#             return self.form_valid(form)
+#         else:
+#             return self.form_invalid(form)
+#
+#
+# def getfiles(request, filenames):
+#     filename = 'zip11.zip'
+#     # filedir = 'results/zip11.zip'
+#     folder = 'results/'
+#     baisc_dir = folder + filename
+#     filepath = os.path.join(settings.MEDIA_ROOT, folder)
+#     with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
+#         for image_name in filenames:
+#             image_url = filenames[image_name]
+#             url = urllib.request.urlopen(image_url)
+#             # image = Image.open(requests.get(image_url, stream=True).raw)
+#             # img_name = os.path.basename(image_path)
+#             print("url is : " + url)
+#             img_zip.write(url.read(), image_name)
+#
+#     img_zip.close()
+#
+#     file = open(baisc_dir, 'rb')
+#     fs = FileSystemStorage(location=filepath)  # defaults to   MEDIA_ROOT
+#     file_name = "myimages" + str(get_random_string(8)) + ".zip"
+#     file_path_within_bucket = os.path.join("output", file_name)
+#     # final_file = fs.save(filename, file)
+#     # file_url = fs.url(final_file)
+#     media_storage.save(file_path_within_bucket, img_zip)
+#     file_url = media_storage.url(file_path_within_bucket)
+#     print("zip file url is : " + str(file_url))
+#     file.close()
+#     # final_path = "/results" + str(file_url)
+#     request.session['final_file'] = file_url
+#     # return file_url
+#
+#
+# def append_output(image):
+#     fname = os.path.splitext(image.name)[0]
+#     image_name = str(fname) + ".PNG"
+#     output_path = os.path.join(settings.BASE_DIR, 'images', image_name)
+#     return output_path
+#
+#
+# def delete_old_files(folder):
+#     # folder = loc
+#     for filename in os.listdir(folder):
+#         file_path = os.path.join(folder, filename)
+#         try:
+#             if os.path.isfile(file_path) or os.path.islink(file_path):
+#                 os.unlink(file_path)
+#             elif os.path.isdir(file_path):
+#                 shutil.rmtree(file_path)
+#         except Exception as e:
+#             print('Failed to delete %s. Reason: %s' % (file_path, e))
+#
+#
+# def process_image(image):
+#     fname = os.path.splitext(image.name)[0]
+#     image_name = str(fname) + ".PNG"
+#     output_path = os.path.join(settings.BASE_DIR, 'images', image_name)
+#     # print("output_path is : "+ str(output_path))
+#     input = Image.open(image)
+#     output = remove(input)
+#     # format = 'png'
+#     image_path_within_bucket = os.path.join("images", image_name)
+#     print("image_path_within_bucket is : " + image_path_within_bucket)
+#     output.save(output_path)
+#     image = open(output_path, 'rb')
+#     # media_storage.save(image_path_within_bucket, image)
+#     # image_url = media_storage.url(image_path_within_bucket)
+#     # print("image url is : " + str(image_url))
+#     # # bg_removed_images.append(str(output_path))
+#     # print(image_url)
+#
+#
+# def form_handle(request):
+#     form = AddForm()
+#     if request.method == 'POST':
+#         form = AddForm(request.POST, request.FILES or None)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             # now in the object cd, you have the form as a dictionary.
+#             files = request.FILES.getlist('images')
+#             # x = 1
+#             # y = 1
+#             # images_list = {}
+#             bg_removed_images = []
+#             for image in files:
+#                 print()
+#                 bg_removed_images.append(str(append_output(image)))
+#             executor = concurrent.futures.ProcessPoolExecutor(2)
+#             futures = [executor.submit(process_image, item) for item in files]
+#             concurrent.futures.wait(futures)
+#             print('converting was done')
+#             baisc_dir = 'images5.zip'
+#             with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
+#                 for image_path in bg_removed_images:
+#                     print(image_path)
+#                     img_zip.write(image_path, basename(image_path))
+#                 img_zip.close()
+#             print('zipping was done')
+#             file = open(baisc_dir, 'rb')
+#             file_name = "myimages" + get_random_string(8) + ".zip"
+#             file_path_within_bucket = os.path.join("output", file_name)
+#             media_storage.save(file_path_within_bucket, file)
+#             file_url = media_storage.url(file_path_within_bucket)
+#             print('uploading was done')
+#             print("zip file url is : " + str(file_url))
+#             request.session['final_file'] = str(file_url)
+#             output_path = os.path.join(settings.BASE_DIR, 'images')
+#             delete_old_files(output_path)
+#             form = AddForm()
+#             # return HttpResponseRedirect(str(file_url))
+#             return HttpResponseRedirect(reverse('home'))
+#     return render(request, "index.html", {"form": form})
+#
+#
+# def form_handle2(request):
+#     form = AddForm()
+#     if request.method == 'POST':
+#         form = AddForm(request.POST, request.FILES or None)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             # now in the object cd, you have the form as a dictionary.
+#             files = request.FILES.getlist('images')
+#             x = 1
+#             y = 1
+#             images_list = {}
+#             bg_removed_images = []
+#             print("first images are: ")
+#             x = 1
+#             for image in files:
+#                 images_list[x] = image
+#                 x = x + 1
+#             print(images_list)
+#             print(x)
+#             for y in range(y, x, 3):
+#                 y2 = int(y) + 1
+#                 y3 = int(y) + 2
+#                 if y3 < x:
+#                     t1 = threading.Thread(target=process_image, args=(images_list[y],))
+#                     t2 = threading.Thread(target=process_image, args=(images_list[y2],))
+#                     t3 = threading.Thread(target=process_image, args=(images_list[y3],))
+#                     t1.start(), t2.start(), t3.start()
+#                     t1.join(), t2.join(), t3.join()
+#                     bg_removed_images.append(str(append_output(images_list[y])))
+#                     bg_removed_images.append(str(append_output(images_list[y2])))
+#                     bg_removed_images.append(str(append_output(images_list[y3])))
+#                 elif y2 < x:
+#                     t1 = threading.Thread(target=process_image, args=(images_list[y],))
+#                     t2 = threading.Thread(target=process_image, args=(images_list[y2],))
+#                     t1.start(), t2.start()
+#                     t1.join(), t2.join()
+#                     bg_removed_images.append(str(append_output(images_list[y])))
+#                     bg_removed_images.append(str(append_output(images_list[y2])))
+#                 else:
+#                     t1 = threading.Thread(target=process_image, args=(images_list[y],))
+#                     t1.start()
+#                     t1.join()
+#                     bg_removed_images.append(str(append_output(images_list[y])))
+#
+#             print('bg_removed_images is ::')
+#             print(bg_removed_images)
+#             baisc_dir = 'images4.zip'
+#             with zipfile.ZipFile(baisc_dir, 'w') as img_zip:
+#                 for image_path in bg_removed_images:
+#                     img_zip.write(image_path)
+#                 img_zip.close()
+#             file = open(baisc_dir, 'rb')
+#             file_name = "myimages" + get_random_string(8) + ".zip"
+#             file_path_within_bucket = os.path.join("output", file_name)
+#             media_storage.save(file_path_within_bucket, file)
+#             file_url = media_storage.url(file_path_within_bucket)
+#             print("zip file url is : " + str(file_url))
+#             request.session['final_file'] = str(file_url)
+#             form = AddForm()
+#             # return HttpResponseRedirect(str(file_url))
+#             return HttpResponseRedirect(reverse('home'))
+#     return render(request, "index.html", {"form": form})
+#
+#
+# def downlaod_images(request, file):
+#     file_link = file
+#     try:
+#         file_name = request.session['final_file']
+#     except:
+#         file_name = None
+#     if file_name is None:
+#         print("file_name is None")
+#         return HttpResponseRedirect(reverse('home'))
+#     else:
+#         if file_link != file_name:
+#             print("file_name is not equal with session name")
+#             return HttpResponseRedirect(reverse('home'))
+#         else:
+#             filepath = os.path.join(settings.MEDIA_ROOT, "results")
+#             fs = FileSystemStorage(location=filepath)
+#             out_file = fs.path(file_link)
+#             the_file3 = open(out_file, 'rb')
+#             the_file = "/media/results" + str(out_file)
+#             print(" the file is : " + str(the_file))
+#             response = FileResponse(the_file3)
+#
+#             # serv = serve(request, str(the_file))
+#             return response
